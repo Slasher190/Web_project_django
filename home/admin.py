ad1a7330016e73aefae556400f70eb78f831154e
Slasher190 <57698770+Slasher190@users.noreply.github.com>
2020-06-21 13:45:12 +0530

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from home.models import Contact
+# Register your models here.
+
+admin.site.register(Contact)
+#in this field no one can see the code even passwords
